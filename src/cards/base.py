7f469beb1466d5e94e8e92d7083eb13fdce994bd
Richard <chnsoc@gmail.com>
2024-08-08 11:56:07 +0700

    add card initialization from netrunnerdb files

--- src/cards/base.py
+++ src/cards/base.py
@@ -1,13 +1,71 @@
 class Card:
-    def __init__(self, name: str, cost: int, card_type: str):
-        self.name = name
-        self.cost = cost
-        self.type = card_type
+    def __init__(self, card_data):
+        attributes = card_data.get('attributes', {})
+        
+        self.id = card_data.get('id', '')
+        self.name = attributes.get('title', '')
+        self.type = attributes.get('card_type_id', '')
+        self.text = attributes.get('text', '')
+        self.cost = attributes.get('cost')
+        self.faction = attributes.get('faction_id', '')
+        self.side = attributes.get('side_id', '')
+        self.strength = attributes.get('strength')
+        self.stripped_text = attributes.get('stripped_text', '')
+        self.is_unique = attributes.get('is_unique', False)
+        self.subtypes = attributes.get('card_subtype_ids', [])
+        self.influence_cost = attributes.get('influence_cost')
+        self.base_link = attributes.get('base_link')
+        self.deck_limit = attributes.get('deck_limit')
+        self.influence_limit = attributes.get('influence_limit', '')
+        self.advancement_requirement = attributes.get(
+            'advancement_requirement')
+        self.agenda_points = attributes.get('agenda_points')
+        self.trash_cost = attributes.get('trash_cost')
+        self.memory_cost = attributes.get('memory_cost')
+        self.date_release = attributes.get('date_release')
+        self.minimum_deck_size = attributes.get('minimum_deck_size')
+        self.display_subtypes = attributes.get('display_subtypes', "")
+
+        # Add card_abilities
+        self.card_abilities = attributes.get('card_abilities', {})
+        
+        # For easier access, we can also add individual attributes for each ability
+        self.additional_cost = self.card_abilities.get('additional_cost', False)
+        self.advanceable = self.card_abilities.get('advanceable', False)
+        self.gains_subroutines = self.card_abilities.get('gains_subroutines', False)
+        self.interrupt = self.card_abilities.get('interrupt', False)
+        self.link_provided = self.card_abilities.get('link_provided')
+        self.mu_provided = self.card_abilities.get('mu_provided')
+        self.num_printed_subroutines = self.card_abilities.get('num_printed_subroutines', 0)
+        self.on_encounter_effect = self.card_abilities.get('on_encounter_effect', False)
+        self.performs_trace = self.card_abilities.get('performs_trace', False)
+        self.recurring_credits_provided = self.card_abilities.get('recurring_credits_provided')
+        self.rez_effect = self.card_abilities.get('rez_effect', False)
+        self.trash_ability = self.card_abilities.get('trash_ability', False)
 
     def play(self, player, game):
         pass
 
-    
+    def apply_data(self, data):
+        self.id = data.get("id")
+        self.name = data.get("name")
+        self.cost = data.get("cost")
+        self.type = data.get("type")
+        self.components = []
+
+    def add_component(self, component):
+        self.components.append(component)
+
+    def add_event_listener(self, event, callback):
+        if event not in self.event_listeners:
+            self.event_listeners[event] = []
+        self.event_listeners[event].append(callback)
+
+    def trigger(self, event, *args):
+        for listener in self.event_listeners.get(event, []):
+            listener(*args)
+
+
 def gain_credits(amount):
     def effect(player, game):
         player.credits += amount
--- src/cards/card_registry.py
+++ src/cards/card_registry.py
@@ -1,28 +1,20 @@
 import json
-from typing import Dict, Any, Callable
 
-from cards.base import Card
-
+from base import Card
 class CardRegistry:
     def __init__(self):
-        self.cards_data: Dict[str, Dict[str, Any]] = {}
-        self.card_factories: Dict[str, Callable[[], Card]] = {}
+        self.cards = {}
 
-    def load_card_data(self, json_file_path: str):
-        with open(json_file_path, 'r') as file:
-            self.cards_data = json.load(file)
+    def load_cards_from_json(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            card_data = json.load(file)
+            print(f"Loaded {len(card_data['data'])} cards from {file_path}")
+            for card_info in card_data["data"]:
+                card = Card(card_info)
+                self.cards[card.id] = card
 
-    def register_card_factory(self, card_id: str, factory: Callable[[], Card]):
-        self.card_factories[card_id] = factory
+    def get_card(self, card_id):
+        return self.cards.get(card_id)
 
-    def create_card(self, card_id: str) -> Card:
-        if card_id not in self.card_factories:
-            raise ValueError(f"No factory registered for card ID: {card_id}")
-        
-        card_data = self.cards_data.get(card_id)
-        if not card_data:
-            raise ValueError(f"No data found for card ID: {card_id}")
-
-        card = self.card_factories[card_id]()
-        card.apply_data(card_data)
-        return card
+    def get_all_cards(self):
+        return list(self.cards.values())
